4d576eddc1654c193cff8b64dba324bdd2be8806
VignanBaligari234 <114463516+VignanBaligari234@users.noreply.github.com>
2024-04-08 16:20:19 +0530

    added arrays scropt

--- 05.arrays.sh
+++ 05.arrays.sh
@@ -5,3 +5,4 @@ PERSONS=("Ramesh" "suresh" "mahesh")
 echo "First person name is ${PERSONS[0]}"
 echo " Print all the persons: ${PERSONS[@]}" 
 echo " print second person name: ${PERSONS[1]}"
+echo "third person name: ${PERSONS[2]}"
