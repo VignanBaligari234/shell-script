dbda235123e42d0c76dbfde921e16dc10c1e13e6
VignanBaligari234 <114463516+VignanBaligari234@users.noreply.github.com>
2024-04-08 14:44:44 +0530

    added script to hello-world

---
+++ 04.variables-prompt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Please enter your username:"
+
+read USERNAME
+
+echo "entered username is: $USERNAME"
+
+echo "Please enter your password:"
+
+read PASSWORD
+
+echo "entered username is: $PASSWORD"
+
