c246426d04554044ad8aa03d1cc8f39734a62368
VignanBaligari234 <114463516+VignanBaligari234@users.noreply.github.com>
2024-04-08 14:33:11 +0530

    added script to hello-world

--- 03-variables-commandline.sh
+++ 03-variables-commandline.sh
@@ -3,7 +3,7 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "$PERSON1: Hello $PERSON2 good morning!"
+echo "$PERSON1: Hello $PERSON2! good morning!"
 echo "$PERSON2: hello $PERSON1! good morning"
-echo "$PERSON1: how are you $PERSON2 ?!"
-echo "$PERSON2: I am fine $PERSON1 , thank you!"
+echo "$PERSON1: how are you $PERSON2?"
+echo "$PERSON2: I am fine $PERSON1, thank you!"
