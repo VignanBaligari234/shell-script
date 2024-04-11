81f504ca6693dbe91d87b1a148582ba32c011690
VignanBaligari234 <114463516+VignanBaligari234@users.noreply.github.com>
2024-04-11 22:00:20 +0530

    added loops script

---
+++ 08.delete-old-logs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+APP_LOGS_DIR=/home/centos/app-logs
+
+DATE=$(date +%F)
+LOG_FILE_DIRECTORY=/home/centos/shellscript.logs
+SCRIPT_NAME=$0
+LOGFILE=$LOG_FILE_DIRECTORY/$0-$DATE.log
+
+FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
+
+echo "$FILES_TO_DELETE"
