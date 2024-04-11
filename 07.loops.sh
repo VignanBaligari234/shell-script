5f94bd6e049a9e466e5a513decced870c80701fc
VignanBaligari234 <114463516+VignanBaligari234@users.noreply.github.com>
2024-04-11 21:21:27 +0530

    added loops script

--- 07.loops.sh
+++ 07.loops.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 USERID=$(id -u)
+DATE=$(date +%F)
+LOG_FILE_DIRECTORY=/home/centos/shellscript.logs
+SCRIPT_NAME=$0
+LOGFILE=$LOG_FILE_DIRECTORY/$0-$DATE.log
 R="\e[31m"
 N="\e[0m"
 Y="\e[33m"
@@ -11,3 +15,27 @@ then
     echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1
 fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "Installing $2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "Installing $2 ... $G SUCCESS $N"
+    fi
+}
+
+for i in $@
+do 
+    yum list installed $i &>>$LOGFILE 
+    if [ $? -ne 0 ]
+    then
+        echo "$i is not installed, let's install it"
+        yum install $i -y &>>$LOGFILE
+        VALIDATE $? "$i"
+        
+    else
+        echo -e "$Y $i is already installed $N"
+    fi
+done
